43300f5d6ae99b753e992caca1ed49b60686892a
lang <jingyifsy@gmail.com>
2024-12-20 20:51:34 +0800

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
 
-const inter = Inter({
-  subsets: ['latin'],
+const noto = Inter({
+  subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={`${noto.className}`} suppressHydrationWarning>
       <body>
         <RootProvider>{children}</RootProvider>
       </body>
