e5c9dea6b513f40d74194d766354fcb8c44b505b
lang <jingyifsy@gmail.com>
2024-12-25 03:00:04 +0800

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,16 @@ import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import { ReactNode } from "react";
+import type { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "lang books",
+  description: "Go语言相关书籍，从入门到失业",
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
